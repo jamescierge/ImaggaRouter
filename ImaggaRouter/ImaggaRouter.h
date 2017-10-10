0b6a34e6fe89a78b65163ca75d0323814e50642c
James McPherson <james.mcpherson@monscierge.com>
2017-10-10 13:24:05 -0500

    Initial ImaggaRouter

---
+++ ImaggaRouter/ImaggaRouter.h
@@ -0,0 +1,19 @@
+//
+//  ImaggaRouter.h
+//  ImaggaRouter
+//
+//  Created by James McPherson on 10/10/17.
+//  Copyright © 2017 James McPherson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ImaggaRouter.
+FOUNDATION_EXPORT double ImaggaRouterVersionNumber;
+
+//! Project version string for ImaggaRouter.
+FOUNDATION_EXPORT const unsigned char ImaggaRouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ImaggaRouter/PublicHeader.h>
+
+
